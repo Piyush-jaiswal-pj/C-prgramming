b4f9d70f5a9252057f8c311b71c209aac82b9b3c
Piyush <piyushjaiswal976@gmail.com>
2021-08-30 01:16:44 +0530

    average

--- c basic program topic wise/average.c
+++ c basic program topic wise/average.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<stdio.h>
 
 int main()
@@ -9,16 +8,4 @@ int main()
      avg=sum / 3;
     printf("%f",avg);
   return 0;
-=======
-#include<stdio.h>
-
-int main()
-{
-    int a=10,b=11,c=12;
-    float avg,sum;
-     sum=a + b + c;
-     avg=sum / 3;
-    printf("%f",avg);
-  return 0;
->>>>>>> 5c877f6 (all pgrams)
 }
