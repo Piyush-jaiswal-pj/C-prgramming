d9d9f608547a9f0432a14ffaf1a360492d0445fb
Piyush <piyushjaiswal976@gmail.com>
2021-08-30 01:18:13 +0530

    c logic

--- c basic program topic wise/charintoASCII.c
+++ c basic program topic wise/charintoASCII.c
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #include<stdio.h>
 int main()
 {
@@ -8,15 +8,5 @@ int main()
     printf("The ASCII of entered character is : ");
     printf("%d",input);
     return 0;
-=======
-#include<stdio.h>
-int main()
-{
-    char input;
-    printf("Enter any character to know its ASCII value (American Standard Code for Information Interchange \n)");
-    scanf("%c",&input);
-    printf("The ASCII of entered character is : ");
-    printf("%d",input);
-    return 0;
->>>>>>> 5c877f6 (all pgrams)
+
 }
--- c basic program topic wise/fibonicciseries.c
+++ c basic program topic wise/fibonicciseries.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 int main()
 {
@@ -11,22 +10,4 @@ int main()
     }
     
     return 0;
-=======
-#include <stdio.h>
-#include <conio.h>
-void main()
-{
-    int input, i, c, a = 0, b = 1;
-    printf("Enter any number for fibbonicci series \n");
-    scanf("%d", &input);
-    printf(" %d %d ", a, b);
-    for (i = 2; i < input; i++)
-    {
-        c = a + b;
-        printf(" %d ", c);
-        a = b;
-        b = c;
-    }
-    getch();
->>>>>>> 5c877f6 (all pgrams)
 }
